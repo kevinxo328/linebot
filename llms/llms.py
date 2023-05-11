2dabc4d010d09b7efa801cec5788dcb9452fac82
Kevin Liu <kevinxo328@gmail.com>
2023-05-12 07:39:09 +0800

    Feat: use huggingfacehub

--- llms/llms.py
+++ llms/llms.py
@@ -1,11 +1,13 @@
+from langchain import HuggingFaceHub
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-from config import OPENAI_API_KEY
+from config import OPENAI_API_KEY, HUGGINGFACEHUB_API_TOKEN
 
 
 class LLMService:
-    llm = OpenAI(temperature=0.9)
+    repo_id = "google/flan-t5-xl"
+    llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0, "max_length": 64})
     prompt = PromptTemplate(
         input_variables=["product"],
         template="What is a good name for a company that makes {product}?",
@@ -18,3 +20,7 @@ class LLMService:
             return "No product provided"
         else:
             return cls.chain.run(product=product)
+
+
+if __name__ == '__main__':
+    print(LLMService.chain_run(product='Tesla'))
